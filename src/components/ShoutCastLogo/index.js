8e1a5544ecdc0ab32c24088b60e3fd8e390a940e
Camille <aziaziazi@gmail.com>
2017-08-05 14:11:30 +0200

    fix header logo

--- src/components/ShoutCastLogo/index.js
+++ src/components/ShoutCastLogo/index.js
@@ -4,13 +4,22 @@ import styled from 'styled-components';
 const LogoWrapper = styled.div`
   position: relative;
   height: 80px;
-  padding-left: 40px;
+  padding: 0 40px;
+  @media (max-width: 700px) {
+    margin-bottom: 22px;
+  }
 `
 const Img = styled.img`
   position: absolute;
   top: 50%;
   height: 40px;
   margin-top: -20px;
+
+  @media (max-width: 700px) {
+    position: relative;
+    display: block;
+    margin -20px auto 20px auto;
+  }
 `
 
 const ShoutCastLogo = () => (
